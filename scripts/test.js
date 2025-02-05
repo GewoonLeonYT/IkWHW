e4b036d3c9a3ef5189d18d5b0019980b056bb0ed
Leon Grutters <gruttersleonbot2@gmail.com>
2025-02-05 21:15:10 +0100

    Automatically populate form with stations.

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,4 @@
+var apiURL = "https://gateway.apiportal.ns.nl" 
 function getCookie(cookieName) {
     let name = cookieName + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -13,15 +14,53 @@ function getCookie(cookieName) {
     }
     return "";
 }
+                                           
+function makeCookie(cookieName, cookieValue, expirationDuration) {
+    const date = new Date();
+    date.setTime(date.getTime() + (expirationDuration*24*60*60*1000));
+    // console.log(date.toString());
+    let expirationDate = date.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + "expires=" + expirationDate + ";path=/"
+}
 
-{
-    let textElement = document.getElementById("api-key");
+async function retrieveStations() {
+    const date = new Date();
+    let requestParameters = "?includeNonPlannableStations=false";
+    let init = {
+        method: "GET",
+        headers: {
+            'Ocp-Apim-Subscription-Key': token,
+        },
+    }
+ 
+    fetch(apiURL + "/nsapp-stations/v3" + requestParameters, init)
+    .then(response => {
+        return response.text();
+    })
+    .then(data => sessionStorage.setItem("stations", data))
+    .catch(err => console.error(err));
+}
+
+let token = getCookie("has-token")  == "session" ? sessionStorage.getItem("token") : localStorage.getItem("token");
+retrieveStations().then(() => {
+    let stationsFull = JSON.parse(sessionStorage.getItem("stations"));
+    let stationsPayload = stationsFull.payload;
     
-    if (getCookie("has-token") == "session") {
-        var newText = document.createTextNode(sessionStorage.getItem("token"));
-    } else if (getCookie("has-token") == "local") {
-        var newText = document.createTextNode(localStorage.getItem("token"));
+    let stationsForm = document.getElementsByName("station")[0];
+    
+    for (station of stationsPayload) {
+        if (station.country != "NL") continue;
+    
+        let stationName = station.names.long;
+        let stationCode = station.id.code;
+    
+        let option = document.createElement("option");
+        option.setAttribute("value", stationCode);
+        let text = document.createTextNode(`${stationName} (${stationCode})`);
+        option.appendChild(text);
+        
+        stationsForm.appendChild(option);
     }
     
-    textElement.appendChild(newText);
-}
+    stationsForm.removeAttribute("disabled");
+})
