af5bbd3b3c71f1b1d98f0e58a5764e0879d5ea89
Leon Grutters <gruttersleonbot2@gmail.com>
2025-02-12 11:18:44 +0100

    Implement getting furthest station for price.
    Uses an estimated price per fare unit to prevent sending too many requests to NS API.
    If there's multiple routes to a station with differing prices, get the price for the
    route with the lowest price.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,32 @@
-var apiURL = "https://gateway.apiportal.ns.nl" 
+const urlAPI = "https://gateway.apiportal.ns.nl";
+const stationsAPI = "nsapp-stations/v3";
+const pricesAPI = "reisinformatie-api/api/v3/price";
+const tripsAPI = "reisinformatie-api/api/v3/trips"
+const bareFare = 112;
+const farePerFareUnit = 20; // This is delibrately an underestimation.
+let stationsPayload;
+
+let token = getCookie("has-token") == "session" ?
+sessionStorage.getItem("token") : localStorage.getItem("token");
+
+async function callAPI(apiSubPage, requestParameters) {
+    let fullURL = `${urlAPI}/${apiSubPage}?${requestParameters}`;
+    let init = {
+        method: "GET",
+        headers: {
+            'Ocp-Apim-Subscription-Key': token,
+        },
+    }
+
+    const response = await fetch(fullURL, init);
+    return response;
+}
+
+async function getData(dataPath) {
+    const response = await fetch(dataPath);
+    return response.json();
+}
+
 function getCookie(cookieName) {
     let name = cookieName + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -14,40 +42,35 @@ function getCookie(cookieName) {
     }
     return "";
 }
-                                           
+ 
 function makeCookie(cookieName, cookieValue, expirationDuration) {
     const date = new Date();
-    date.setTime(date.getTime() + (expirationDuration*24*60*60*1000));
+    date.setTime(date.getTime() + (expirationDuration * 24 * 60 * 60 * 1000));
     // console.log(date.toString());
     let expirationDate = date.toUTCString();
-    document.cookie = cookieName + "=" + cookieValue + ";" + "expires=" + expirationDate + ";path=/"
+    document.cookie = cookieName + "=" + cookieValue + ";" + "expires="
+    + expirationDate + ";path=/"
 }
 
-function retrieveStations() {
-    const date = new Date();
-    let requestParameters = "?includeNonPlannableStations=false";
-    let init = {
-        method: "GET",
-        headers: {
-            'Ocp-Apim-Subscription-Key': token,
-        },
-    }
- 
-    fetch(apiURL + "/nsapp-stations/v3" + requestParameters, init)
-    .then(response => response.json())
-    .then(data => populateStations(data));
-}
+// function populateStations(stationsFull) {
+    
+//     return stationsPayload;
+// }
 
-function populateStations(stationsFull) {
-    let stationsPayload = stationsFull.payload;
+document.addEventListener("DOMContentLoaded", async () => {
+    let requestParameters = "includeNonPlannableStations=false";
+    let stationsResponse = await callAPI(stationsAPI, requestParameters);
+    let stationsFull = await stationsResponse.json();
+
+    stationsPayload = stationsFull.payload;
 
     stationsPayload.sort((a, b) => a.names.long.localeCompare(b.names.long))
-    
+
     let stationsForm = document.getElementById("station");
-    
+
     for (station of stationsPayload) {
         if (station.country != "NL") continue;
-    
+
         let stationNameLong = station.names.long;
         let stationCode = station.id.code;
 
@@ -56,9 +79,73 @@ function populateStations(stationsFull) {
         option.setAttribute("label", `${stationNameLong} (${stationCode})`);
         stationsForm.appendChild(option);
     }
-    
-    stationsForm.removeAttribute("disabled")
-}
 
-let token = getCookie("has-token")  == "session" ? sessionStorage.getItem("token") : localStorage.getItem("token");
-retrieveStations();
+    stationsForm.removeAttribute("disabled");
+})
+
+document.addEventListener("submit", async e => {
+    e.preventDefault();
+    let form = e.target;
+
+    let formData = new FormData(form);
+    let stationCode = formData.get("station");
+    let budgetInCents = Number(formData.get("budget")).toFixed(2)*100;
+
+    let station = stationsPayload.find(station => station.id.code ==
+        stationCode);
+    // let stationNameLong = station.names.long;
+
+    let foundFurthestStation = false;
+
+    let distances = await getData(`./data/distances/${stationCode}.json`);
+    let fare_units = await getData(`./data/fare_units/${stationCode}.json`);
+
+    for (let toStationCode in distances) {
+        let estimatedPrice = bareFare + (farePerFareUnit * fare_units[toStationCode]);
+        let actualPrice;
+        let trip;
+        if (estimatedPrice > budgetInCents || toStationCode == stationCode)  continue;
+        let toStation = stationsPayload.find(station => station.id.code ==
+            toStationCode);
+        let toStationNameLong = toStation.names.long;
+        let distance = distances[toStationCode];
+        let priceRequestParameters = `fromStation=${stationCode}&toStation=`
+        + `${toStationCode}&adults=1&children=0&travelClass=SECOND_CLASS`
+        + `&travelType=single`;
+        let tripsRequestParameters = `fromStation=${stationCode}&toStation=`
+        + `${toStationCode}`
+        let priceResponse = await callAPI(pricesAPI, priceRequestParameters);
+        let priceFull = await priceResponse.json();
+
+        if (priceResponse.status == 200) {
+            actualPrice = priceFull.payload.prices[0].totalPriceInCents;
+        } else if (priceResponse.status == 500
+            && priceFull.errors[0].message == 
+            "Multiple routes are possible with different prices." ) {
+            let tripsResponse = await callAPI(tripsAPI, tripsRequestParameters);
+            let tripsFull = await tripsResponse.json();
+            let trips = tripsFull.trips;
+            let shortestRouteId;
+            
+            for (trip of trips) {
+                // console.log(trip);
+                let tripPrice = trip.productFare.priceInCents;
+                let routeId = trip.fareRoute.routeId;
+                if (actualPrice === undefined || tripPrice < actualPrice) {
+                    actualPrice = tripPrice;
+                    shortestRouteId = routeId;
+                }
+            }
+        }
+        if (actualPrice <= budgetInCents) {
+            alert(`The furthest station is ${toStationNameLong}, which is ${distance.toFixed(2)} km away and costs ${(actualPrice / 100).toLocaleString({}, {style: "currency",
+            currency:"EUR"})}`);
+            foundFurthestStation = true;
+            break;
+        }
+
+    }
+    if (!foundFurthestStation) {
+        alert("You cannot travel to any station :c");
+    }    
+})
