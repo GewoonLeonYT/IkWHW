fc10c2912dec4ea68ef8022e7b1f2702780a1fff
Leon Grutters <gruttersleonbot2@gmail.com>
2025-02-12 11:12:31 +0100

    Minor formatting changes

--- scripts/token.js
+++ scripts/token.js
@@ -1,6 +1,6 @@
 function makeCookie(cookieName, cookieValue, expirationDuration) {
     const date = new Date();
-    date.setTime(date.getTime() + (expirationDuration*24*60*60*1000));
+    date.setTime(date.getTime() + (expirationDuration * 24 * 60 * 60 * 1000));
     // console.log(date.toString());
     let expirationDate = date.toUTCString();
     document.cookie = cookieName + "=" + cookieValue + ";" + "expires=" + expirationDate + ";path=/"
@@ -15,7 +15,7 @@ function makeTempCookie(cookieName, cookieValue) {
 document.getElementById("token-block-form").addEventListener("submit", e => {
     e.preventDefault();
     var form = e.target;
-    
+
     var formData = new FormData(form);
     var token = formData.get("token");
     var remember = formData.get("remember-token");
