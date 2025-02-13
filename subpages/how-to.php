e049ba538acddcd9fa085ad413837bf1fb7fddf3
Leon Grutters <gruttersleonbot2@gmail.com>
2025-02-13 22:21:58 +0100

    Add how to page

---
+++ subpages/how-to.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="./stylesheets/global.css">
+        <title>IkWHW</title>
+        <link rel="icon" type="image/svg+xml" href="./content/favicon.svg">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+    <div class="header">
+        <a href="." class="logo">    
+            <img src="./content/icon.svg">
+            IkWHW
+        </a>
+        <a href="how-to" class="active">
+            How to
+        </a>
+    </div>
+    <div class="centered rounded box shadow bordered">
+        <p>
+            In order to use this website, you need an NS API key.<br>
+            This can be acquired via the <a href="https://apiportal.ns.nl">NS API Portal</a>.
+        </p>
+    </div>
+    </body>
+</html>
