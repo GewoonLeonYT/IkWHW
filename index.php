08b3dca0a7b8f5e6107f3a2c212a76bee3c853f0
Leon Grutters <gruttersleonbot2@gmail.com>
2025-02-13 22:21:47 +0100

    Add navbar

--- index.php
+++ index.php
@@ -7,14 +7,25 @@
     <head>
         <?php
             foreach ($styles as $style) {
-                echo '<link rel="stylesheet" href="stylesheets/' . $style . '.css">';
+                echo '<link rel="stylesheet" href="./stylesheets/' . $style . '.css">';
             }
         ?>
         <title>IkWHW</title>
+        <link rel="icon" type="image/svg+xml" href="./content/favicon.svg">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
+    <div class="header">
+        <a href="." class="active logo">    
+            <img src="./content/icon.svg">
+            IkWHW
+        </a>
+        <a href="./how-to">
+            How to
+        </a>
+    </div>
         <?php
-            include "subpages/" . $loadPage . ".html";
+            include "./subpages/" . $loadPage . ".html";
         ?>
     </body>
 </html>
