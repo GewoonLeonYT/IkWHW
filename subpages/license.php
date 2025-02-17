1e7c88f7f94024f2e6ed041d74b241aabdbe4d4c
Leon Grutters <gruttersleonbot2@gmail.com>
2025-02-17 09:09:19 +0100

    Update license.php

--- subpages/license.php
+++ subpages/license.php
@@ -7,35 +7,34 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-    <div class="header">
-        <a href="." title="IkWilHierWeg" class="logo">    
-            <img src="./content/icon.svg">
-            IkWHW
-        </a>
-        <a title="How to" href="how-to">
-            How to
-        </a>
-        <a href="license" title="License" class="active">
-            License
-        </a>
-        <a title="Github" class="right github-logo-link" target="_blank"  href="https://github.com/GewoonLeonYT/IkWHW">
-            <img class="github-logo" src="./content/github-mark.svg">
-        </a>
-    </div>
-    <div class="centered rounded box shadow bordered">
-        <p>
-            All files on this website are licensed under the GNU Affero General Public License
-            version 3 or (at your option) any later version, unless otherwise specified.<br>
-            All files listed are <strong>NOT</strong> licensed under AGPL-3.0-or-later.
-            <?php
-                include "../content/COPYING.html";
-            ?>
-            <li>
-                All files contained in the <code>data</code> sub-directory.
-            </li>
-        </ul>
-        </p>
-
-    </div>
+        <div class="header">
+            <a href="." title="IkWilHierWeg" class="logo">    
+                <img src="./content/icon.svg">
+                IkWHW
+            </a>
+            <a title="How to" href="how-to">
+                How to
+            </a>
+            <a href="license" title="License" class="active">
+                License
+            </a>
+            <a title="Github" class="right github-logo-link" target="_blank"  href="https://github.com/GewoonLeonYT/IkWHW">
+                <img class="github-logo" src="./content/github-mark.svg">
+            </a>
+        </div>
+        <div class="centered rounded box shadow bordered">
+            <span>
+                All files on this website are licensed under the GNU Affero General Public License
+                version 3 or (at your option) any later version, unless otherwise specified.<br>
+                All files listed are <strong>NOT</strong> licensed under AGPL-3.0-or-later.
+                <?php
+                    include "../content/COPYING.html";
+                ?>
+                    <li>
+                        All files contained in the <code>data</code> sub-directory.
+                    </li>
+                </ul>
+            </span>
+        </div>
     </body>
 </html>
